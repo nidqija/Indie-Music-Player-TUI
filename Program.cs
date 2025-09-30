c3430a61ce40dcc7e2c69baa9581e368500f6e40
Raziq Din <144256646+nidqija@users.noreply.github.com>
2025-09-30 19:45:28 +0800

    init main file for music player

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+// this is a music input program
+
+
+
+class MusicChoices
+{
+    string[] menuChoices = { "1. Search Songs", "2. Playlists", "3. Search by Artists" };
+    
+    
+   public void getChoices()
+    {
+        for (int i = 0; i < menuChoices.Length; i++)
+        {
+            Console.WriteLine(menuChoices[i]);
+        }
+    }
+    
+}
+class MusicInput
+{
+    string input = "";
+    string intro = "Welcome to MusicInput Station!";
+
+
+    static void Main(string[] args)
+    {
+        MusicInput musicInput = new MusicInput();
+        musicInput.input = "Hello from MusicInput";
+        Console.WriteLine(musicInput.intro);
+        Console.WriteLine(musicInput.input);
+
+        // display choices
+        MusicChoices musicChoices = new MusicChoices();
+        musicChoices.getChoices();
+
+
+    }
+}
