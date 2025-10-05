f0f0efec049707e8b044f7dcfb0f3e942731da67
Raziq Din <144256646+nidqija@users.noreply.github.com>
2025-10-05 19:28:30 +0800

    added return to main menu code logic

--- obj/Debug/net8.0/MusicPlayer.AssemblyInfo.cs
+++ obj/Debug/net8.0/MusicPlayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cffe5cad648f87a1aa89089319ac1f2dfd519e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0cf614cd00d61d8be002ae06fe28a266014ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
